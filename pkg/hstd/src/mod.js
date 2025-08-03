9e4a212651a3122b31fe6b473b3ae5efb2eb0f1e
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-08-04 02:53:59 +0900

    wip

--- pkg/hstd/src/mod.js
+++ pkg/hstd/src/mod.js
@@ -3,5 +3,6 @@ export * from "./h.js"
 export * from "./on.js"
 export * from "./css.js"
 export * from "./io.js"
-export * from "./prop.js";
+
 export * from "./cache.js";
+export * from "./prop.js";
--- pkg/hstd/src/on.js
+++ pkg/hstd/src/on.js
@@ -1,6 +1,7 @@
-import { Prop } from "./core/prop.js";
-import { listen } from "./core/listen.js";
 import { Cache } from "./core/cache.js";
+import { Prop } from "./core/prop.js";
+
+import { listen } from "./core/listen.js";
 
 const
 
